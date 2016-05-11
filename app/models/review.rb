c6cc82cf4c18eeb9754e3dc81549abc306787025
Romain <romain.cky@gmail.com>
2016-05-11 12:28:59 +0200

    validates the models

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,3 +1,7 @@
 class Review < ActiveRecord::Base
   belongs_to :restaurant
+
+  validates :restaurant, presence: true
+  validates :content, presence: true
+  validates :rating, numericality: { only_integer: true, less_than_or_equal_to: 5, greater_than_or_equal_to: 0, allow_nil: false }
 end
